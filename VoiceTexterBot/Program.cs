d3496562b8c3d4d3e0229e32bcc90200c5db41aa
Morgul3000 <shokuave3000@gmail.com>
2022-10-26 12:05:11 +0300

    Add project files.

---
+++ VoiceTexterBot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using VoiceTexterBot;
+
+Console.OutputEncoding = Encoding.Unicode;
+
+// Объект, отвечающий за постоянный жизненный цикл приложения
+var host = new HostBuilder()
+    .ConfigureServices((hostContext, services) => ConfigureServices(services)) //Задаем конфигурацию
+    .UseConsoleLifetime()  //Позволяет поддерживать приложение активным в консоли
+    .Build();          //Собираем
+
+Console.WriteLine("Сервис запущен.");
+//Запускаем сервис
+await host.RunAsync();
+Console.WriteLine("Сервис остановлен.");
+ 
+
+static void ConfigureServices(IServiceCollection services)
+{
+    //Регистрируем telegramBotClient с токеном подключения
+    services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("5636623417:AAFCfq375c2qQ5LrTatZqa_TPbnG9JHmb28"));
+
+    //Регистрируем постоянно активный сервис бота
+    services.AddHostedService<Bot>();
+}
